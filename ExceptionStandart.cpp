603d7a69a4bbe3748abbef17c1aa6401bfb93c24
haifaamalinawahdan <haifanew3@gmail.com>
2026-06-01 13:12:28 +0700

    implementasi try berisikan deklarasi array berindex 3

--- ExceptionStandart.cpp
+++ ExceptionStandart.cpp
@@ -6,4 +6,10 @@ using namespace std;
 int maiin()
 {
     cout << "Awal program" << endl; //penanda 1
+    try
+    {
+
+        //deklarasi array data dengan panjang index 3
+        array <int, 3> data = { 1, 2, 3};
+    }
 }
